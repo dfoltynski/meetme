72e5a8afc2d8655b98c00535ecfce0acf8129e46
dfoltynski <dawidfoltynksi7@gmail.com>
2020-09-07 08:09:45 +0200

    Register validation on client side

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,138 +11,135 @@ const fs = require("fs");
 const salt = 10;
 
 const generateToken = (user) => {
-    const payload = {
-        id: user._id,
-        name: user.name,
-    };
-    const token = jwt.sign(payload, process.env.SECRET, {
-        expiresIn: "1m",
-    });
+  const payload = {
+    id: user._id,
+    name: user.name,
+  };
+  const token = jwt.sign(payload, process.env.SECRET, {
+    expiresIn: "1m",
+  });
 
-    return token;
+  return token;
 };
 
 exports.loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (await bcrypt.compare(password, user.password)) {
-        const token = generateToken(user);
-        console.log(token);
-        res.header("Bearer-Authorization", token);
-        res.json({ message: "zalogowan", token });
-    }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = generateToken(user);
+    console.log(token);
+    res.header("Bearer-Authorization", token);
+    res.json({ message: "zalogowan", token });
+  }
 
-    res.json("login");
+  res.json("login");
 };
 
 exports.getUser = async (req, res) => {
-    const user = await User.find();
-    res.json(user);
+  const user = await User.find();
+  res.json(user);
 };
 
 const loginValidationSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-    name: Joi.string().min(2).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  name: Joi.string().min(2).required(),
 });
 
 exports.getSpecificUser = async (req, res) => {
-    try {
-        const validatedData = await loginValidationSchema.validateAsync(
-            ({ email, password, name } = req.body)
-        );
+  try {
+    const validatedData = await loginValidationSchema.validateAsync(
+      ({ email, password, name } = req.body)
+    );
 
-        const user = await User.findOne({ name: req.params.name });
-        if (user) {
-            res.json(user);
-        } else {
-            throw new Error("user is not in database");
-        }
-    } catch (error) {
-        console.log(error);
-        const errorMessage = error.details
-            ? error.details[0].message
-            : "Something went wrong";
-        res.json(errorMessage);
+    const user = await User.findOne({ name: req.params.name });
+    if (user) {
+      res.json(user);
+    } else {
+      throw new Error("user is not in database");
     }
+  } catch (error) {
+    console.log(error);
+    const errorMessage = error.details
+      ? error.details[0].message
+      : "Something went wrong";
+    res.json(errorMessage);
+  }
 };
 
 const registerValidationSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-    name: Joi.string().min(2).required(),
-    sex: Joi.string().required(),
-    relationship_status: Joi.string(),
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  name: Joi.string().min(2).required(),
+  sex: Joi.string().required(),
 });
 
 let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
+  destination: function (req, file, cb) {
+    cb(null, "public");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
 let upload = multer({ storage }).single("file");
 
 exports.postUser = (req, res) => {
-    try {
-        upload(req, res, async (err) => {
-            if (err instanceof multer.MulterError) {
-                return res.status(500).json(err);
-            } else if (err) {
-                return res.status(500).json(err);
-            }
+  try {
+    upload(req, res, async (err) => {
+      if (err instanceof multer.MulterError) {
+        return res.status(500).json(err);
+      } else if (err) {
+        return res.status(500).json(err);
+      }
 
-            let imageData = fs.readFileSync(req.file.path);
-            console.log(imageData);
+      let imageData = fs.readFileSync(req.file.path);
+      console.log(imageData);
 
-            console.log(req.file.mimetype);
+      console.log(req.file.mimetype);
 
-            // i done it that way cus email is not defined'
-            const validatedData = await registerValidationSchema.validateAsync({
-                email: req.body.email,
-                password: req.body.password,
-                name: req.body.name,
-                sex: req.body.sex,
-                relationship_status: req.body.relationship_status,
-            });
+      // i done it that way cus email is not defined'
+      const validatedData = await registerValidationSchema.validateAsync({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        sex: req.body.sex,
+      });
 
-            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-            const { email, name, sex, relationship_status } = validatedData;
+      const { email, name, sex } = validatedData;
 
-            const userPic = new userProfilePicture({
-                name: req.file.originalname,
-                type: req.file.mimetype,
-                data: imageData,
-            });
+      const userPic = new userProfilePicture({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        data: imageData,
+      });
 
-            const user = new User({
-                email,
-                password: hashedPassword,
-                name,
-                age: req.body.age,
-                profile_pic: userPic._id,
-                sex,
-                relationship_status,
-            });
+      const user = new User({
+        email,
+        password: hashedPassword,
+        name,
+        age: req.body.age,
+        profile_pic: userPic._id,
+        sex,
+      });
 
-            userPic.save();
-            user.save();
-            res.json("user added");
-        });
-    } catch (error) {
-        console.log(error);
-        const errorMessage = error.details
-            ? error.details[0].message
-            : "Something went wrong";
-        res.json(errorMessage);
-    }
+      userPic.save();
+      user.save();
+      res.json("user added");
+    });
+  } catch (error) {
+    console.log(error);
+    const errorMessage = error.details
+      ? error.details[0].message
+      : "Something went wrong";
+    res.json(errorMessage);
+  }
 };
 
 exports.secretInfo = (req, res) => {
-    res.json("secret");
+  res.json("secret");
 };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,30 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    name: { type: String, required: true },
-    age: { type: Date, required: true },
-    profile_pic: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "userProfilePicture",
-    },
-    friends: [],
-    pin: {},
-    sex: { type: String },
-    relationship_status: { type: String },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  age: { type: Date, required: true },
+  profile_pic: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "userProfilePicture",
+  },
+  friends: [],
+  pin: {},
+  sex: { type: String },
 });
 
 const userProfilePictureSchema = new Schema({
-    name: String,
-    type: String,
-    data: Buffer,
+  name: String,
+  type: String,
+  data: Buffer,
 });
 
 const User = mongoose.model("user", userSchema);
 const userProfilePicture = mongoose.model(
-    "userProfilePicture",
-    userProfilePictureSchema
+  "userProfilePicture",
+  userProfilePictureSchema
 );
 
 module.exports = { User, userProfilePicture };
