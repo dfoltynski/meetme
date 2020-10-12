cfc1ce9f693e9d07fec389ff2dbc932610565280
DziadDawid <53613664+DziadDawid@users.noreply.github.com>
2020-10-12 16:31:19 +0200

    RWD LandingPage

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -4,39 +4,57 @@ import Background from "../background_landing_page_meetme.svg";
 import { WelcomePage, ShowoffPage } from "./styledcomponents";
 import "../App.css";
 const LandingPage = () => {
-  return (
-    <React.Fragment>
-      <WelcomePage bg={Background}>
-        <div className="welcome__text">
-          MEETME
-          <div className="welcome__text__shortdesc">
-            Meet people all around the world, <b>if you are bored...</b>
-          </div>
-          <Link to="/register">
-            <button className="joinus__button">Join us!</button>
-          </Link>
+    return (
+        <div>
+            <WelcomePage bg={Background}>
+                <div className="welcome__text">
+                    MEETME
+                    <div className="welcome__text__shortdesc">
+                        Meet people all around the world,{" "}
+                        <b>if you are bored...</b>
+                    </div>
+                    <Link to="/register">
+                        <button className="joinus__button">Join us!</button>
+                    </Link>
+                </div>
+                <div
+                    className="scrolldown_container"
+                    onClick={() =>
+                        document.getElementById("showoff_page").scrollIntoView({
+                            behavior: "smooth",
+                        })
+                    }
+                >
+                    <div className="scrolldown_container__circle"></div>
+                </div>
+            </WelcomePage>
+            <ShowoffPage id="showoff_page">
+                <div className="showoff_text">This is how it looks like</div>
+                <div className="showoff">
+                    <img
+                        src="https://i.imgur.com/8xR8k4y.png"
+                        className="showoff_picture"
+                        alt="showoff_picture"
+                    />
+                    <img
+                        src="https://i.imgur.com/aRsaomM.png"
+                        className="showoff_picture"
+                        alt="showoff_picture"
+                    />
+                    <img
+                        src="https://i.imgur.com/2MECa7o.png"
+                        className="showoff_picture"
+                        alt="showoff_picture"
+                    />
+                    <img
+                        src="https://i.imgur.com/ecUxXbB.png"
+                        className="showoff_picture"
+                        alt="showoff_picture"
+                    />
+                </div>
+            </ShowoffPage>
         </div>
-        <div
-          className="scrolldown_container"
-          onClick={() =>
-            document.getElementById("showoff_page").scrollIntoView({
-              behavior: "smooth",
-            })
-          }
-        >
-          <div className="scrolldown_container__circle"></div>
-        </div>
-      </WelcomePage>
-      <ShowoffPage id="showoff_page">
-        <div className="showoff_text">This is how it looks like</div>
-        <div className="showoff">
-          <div className="showoff_picture">Replace with a picture</div>
-          <div className="showoff_picture">Replace with a picture</div>
-          <div className="showoff_picture">Replace with a picture</div>
-        </div>
-      </ShowoffPage>
-    </React.Fragment>
-  );
+    );
 };
 
 export default LandingPage;
